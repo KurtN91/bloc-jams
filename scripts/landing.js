0e7d9fe8d951cd24d4349423df13169ca86bfb94
Kurt Neaderhiser <kurt.neaderhiser@gmail.com>
2017-01-31 17:03:16 -0500

    checkpoint 10: Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,13 @@
  var pointsArray = document.getElementsByClassName('point');
- 
+
+ var revealPoint = function(point){
+     point.style.opacity = 1;
+     point.style.transform = "scaleX(1) translateY(0)";
+     point.style.msTransform = "scaleX(1) translateY(0)";
+     point.style.WebkitTransform = "scaleX(1) translateY(0)";
+}
+
  var animatePoints = function(points) {
-
-                var revealPoint = function(arrayIndex){
-                     arrayIndex.style.opacity = 1;
-                     arrayIndex.style.transform = "scaleX(1) translateY(0)";
-                     arrayIndex.style.msTransform = "scaleX(1) translateY(0)";
-                     arrayIndex.style.WebkitTransform = "scaleX(1) translateY(0)";
-                }
-
             forEach(points, revealPoint);
  };
 animatePoints(pointsArray);
@@ -17,11 +16,9 @@ window.onload = function() {
          if (window.innerHeight > 950) {
          animatePoints(pointsArray);
      }
-     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
      window.addEventListener("scroll", function(event) {
-             if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             if (pointsArray[0].getBoundingClientRect().top <= 500) {
              animatePoints(pointsArray);   
          }
      });
