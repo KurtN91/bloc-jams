5428ed23626d24c835ef8885dbcf5481263dac07
Kurt Neaderhiser <kurt.neaderhiser@gmail.com>
2017-02-01 18:51:42 -0500

    Checkpoint 13 rewrite: done

--- scripts/album.js
+++ scripts/album.js
@@ -84,6 +84,12 @@ var albumGriseldi = {
 var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
+        if (currentParent == null){
+            console.log("no parent found");
+        }
+        if (currentParent != targetClass){
+            console.log("No parent found with that class name");
+        }
         while (currentParent.className != targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
