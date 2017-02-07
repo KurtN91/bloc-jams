79dc560e68fabfe4a71d81f79f32362aacb83f8b
Kurt Neaderhiser <kurt.neaderhiser@gmail.com>
2017-02-07 14:09:00 -0500

    Checkpoint 19 done

--- scripts/album.js
+++ scripts/album.js
@@ -1,49 +1,14 @@
- var albumPicasso = {
-     title: 'The Colors',
-     artist: 'Pablo Picasso',
-     label: 'Cubism',
-     year: '1881',
-     albumArtUrl: 'assets/images/album_covers/01.png',
-     songs: [
-         { title: 'Blue', duration: '4:26' },
-         { title: 'Green', duration: '3:14' },
-         { title: 'Red', duration: '5:01' },
-         { title: 'Pink', duration: '3:21'},
-         { title: 'Magenta', duration: '2:15'}
-     ]
- };
- 
- // Another Example Album
- var albumMarconi = {
-     title: 'The Telephone',
-     artist: 'Guglielmo Marconi',
-     label: 'EM',
-     year: '1909',
-     albumArtUrl: 'assets/images/album_covers/20.png',
-     songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
-     ]
- };
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+ var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+ var playerBarPlayButton = '<span class="ion-play"></span>';
+ var playerBarPauseButton = '<span class="ion-pause"></span>';
 
-var albumGriseldi = {
-    title: 'Thubtumping',
-    artist: "Cumbawomba Griseldi",
-    label: 'Snirklethorp',
-    year: '820',
-    albumArtUrl: 'assets/images/album_covers/04.png',
-    songs: [
-            {title: 'The letter 9', duration: '32:01'},
-            {title: 'Boofism', duration: '0:09'},
-            {title: 'Rocks in the watermelon', duration: '83:30'},
-            {title: 'Watching the radio', duration: '3:59'},
-            {title: 'Glass Meat Clocks', duration: '2:12'},
-            {title: 'Song of the yarbong', duration: 'salmon'}
-    ]
-};
+ var currentlyPlayingSongNumber = null;
+ var currentSongFromAlbum = null;
+ var currentAlbum = null;
+
+ var $previousButton = $('.main-controls .previous');
+ var $nextButton = $('.main-controls .next');
 
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
@@ -57,36 +22,40 @@ var albumGriseldi = {
      var $row = $(template);
      
     var clickHandler = function() {
-	var songNumber = $(this).attr('data-song-number');
+	var songNumber = parseInt($(this).attr('data-song-number'));
 
-	if (currentlyPlayingSong !== null) {
-		var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-		currentlyPlayingCell.html(currentlyPlayingSong);
-	}
-	if (currentlyPlayingSong !== songNumber) {
++    if (currentlyPlayingSongNumber !== null) {
++    var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
++             currentlyPlayingCell.html(currentlyPlayingSongNumber);
+}
+	if (currentlyPlayingSongNumber !== songNumber) {
 		$(this).html(pauseButtonTemplate);
-		currentlyPlayingSong = songNumber;
-	} else if (currentlyPlayingSong === songNumber) {
-		$(this).html(playButtonTemplate);
-		currentlyPlayingSong = null;
++        currentlyPlayingSongNumber = songNumber;
++        currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+         updatePlayerBarSong();
++    } else if (currentlyPlayingSongNumber === songNumber) {
+         $(this).html(playButtonTemplate);
+         $('.main-controls .play-pause').html(playerBarPlayButton);
++        currentlyPlayingSongNumber = null;
++        currentSongFromAlbum = null;
 }
      };
  
      
     var onHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
-        var songNumber = songNumberCell.attr('data-song-number');
+        var songNumber = parseInt(songNumberCell.attr('data-song-number'));
 
-        if (songNumber !== currentlyPlayingSong) {
+        if (songNumber !== currentlyPlayingSongNumber) {
             songNumberCell.html(playButtonTemplate);
         }
     };
 
     var offHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
-        var songNumber = songNumberCell.attr('data-song-number');
+        var songNumber = parseInt(songNumberCell.attr('data-song-number'));
 
-        if (songNumber !== currentlyPlayingSong) {
+        if (songNumber !== currentlyPlayingSongNumber) {
             songNumberCell.html(songNumber);
         }
 };
@@ -105,6 +74,8 @@ var albumGriseldi = {
      var $albumSongList = $('.album-view-song-list');
 
  var setCurrentAlbum = function(album) {
+     
+     currentAlbum = album;
      // #1
      // #2
      $albumTitle.text(album.title);
@@ -120,17 +91,55 @@ var albumGriseldi = {
          $albumSongList.append($newRow);     }
 
  };
+     
+var trackIndex = function(album, song) {
+     return album.songs.indexOf(song);
+ };
 
 
+var updatePlayerBarSong = function() {
 
+    $('.currently-playing .song-name').text(currentSongFromAlbum.title);
+    $('.currently-playing .artist-name').text(currentAlbum.artist);
+    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
+    $('.main-controls .play-pause').html(playerBarPauseButton);
 
- var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
- var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+};
+var nextSong = function() {
+    
+    var getLastSongNumber = function(index) {
+        return index == 0 ? currentAlbum.songs.length : index;
+    };
+    
+    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+    currentSongIndex++;
+    
+    if (currentSongIndex >= currentAlbum.songs.length) {
+        currentSongIndex = 0;
+    }
+    
+    currentlyPlayingSongNumber = parseInt(currentSongIndex + 1);
+    currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
 
- var currentlyPlayingSong = null;
+    $('.currently-playing .song-name').text(currentSongFromAlbum.title);
+    $('.currently-playing .artist-name').text(currentAlbum.artist);
+    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.title);
+    $('.main-controls .play-pause').html(playerBarPauseButton);
+    
+    var lastSongNumber = getLastSongNumber(currentSongIndex);
+    var $nextSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+    var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+    
+    $nextSongNumberCell.html(pauseButtonTemplate);
+    $lastSongNumberCell.html(lastSongNumber);
+    
+};
+
  
  $(document).ready(function() {
      setCurrentAlbum(albumPicasso);
+     $previousButton.click(previousSong);
+     $nextButton.click(nextSong);
 
      });
      
@@ -146,11 +155,11 @@ var albumGriseldi = {
           for (var i = 0; i < songRows.length; i++) {
          songRows[i].addEventListener('mouseleave', function(event) {
               var songItem = getSongItem(event.target);
-             var songItemNumber = songItem.getAttribute('data-song-number');
+             var songItemNumber = parseInt(songItem.getAttribute('data-song-number'));
  
              // #2
              if (songItemNumber !== currentlyPlayingSong) {
-                 songItem.innerHTML = songItemNumber;
+                 songItem.innerHTML = parseInt(songItemNumber);
              }
          });
               
